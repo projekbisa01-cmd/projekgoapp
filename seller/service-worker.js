57502606272660fccf86726fe2e6a92788e5dac8
projekbisa01-cmd <projekbisa01@gmail.com>
2026-05-30 19:03:02 +0700

    Update service-worker.js

--- seller/service-worker.js
+++ seller/service-worker.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = 'projekgo-mitra-v1';
+const CACHE_NAME = 'projekgo-mitra-v2';
 const urlsToCache = [
-  '/seller/',
-  '/seller/index.html'
+  './',
+  './index.html'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Memaksa Service Worker langsung aktif
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim()); // Langsung mengambil alih kontrol halaman
 });
 
 self.addEventListener('fetch', event => {
